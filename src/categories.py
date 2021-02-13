4b023c0e05f976349ec8fcd8fb8e29b6a6291c17
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2021-02-13 12:04:48 +0100

    feat(main): display music categories

--- src/categories.py
+++ src/categories.py
@@ -17,3 +17,24 @@ categories = [
     'toplists',
     'travel'
 ]
+
+
+def translate(category_id: str) -> str:
+    translation_map = {
+        "2020": "2020",
+        "alternative": "Alternatif",
+        "at_home": "À la maison",
+        "french_variety": "Variété française",
+        "in_the_car": "Dans la voiture",
+        "hiphop": "Hip-hop",
+        "party": "En soirée",
+        "pop": "Pop",
+        "popculture": "Pop culture",
+        "reggae": "Reggae",
+        "rnb": "RNB",
+        "romance": "Romantique",
+        "rock": "Rock",
+        "toplists": "Charts",
+        "travel": "Voyage"
+    }
+    return translation_map[category_id]
